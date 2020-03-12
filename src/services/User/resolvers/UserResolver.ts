c06032c480ad7f459322e519506e161d1ae289fd
Kaousheik <kaousheik@gmail.com>
2020-03-12 07:09:44 +0530

    login mutation and me query added

--- src/services/User/resolvers/UserResolver.ts
+++ src/services/User/resolvers/UserResolver.ts
@@ -1,10 +1,12 @@
-import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { Resolver, Query, Mutation, Arg, Ctx } from "type-graphql";
 import { InjectRepository } from "typeorm-typedi-extensions";
 import { User } from "../entities/User";
 import { Repository } from "typeorm";
 import { CreateUserInput } from "../inputs/CreateUserInput";
 import bcrypt from "bcryptjs"
 import { Department } from "../../Department/entities/Department";
+import { LoginInput } from "../inputs/LoginInput";
+import { AuthContext } from "../types/AuthContext"
 @Resolver()
 export class UserResolver {
     constructor(
@@ -13,11 +15,21 @@ export class UserResolver {
     ){}
     @Query(() => [User])
     async getAllUsers() {
-        return await this.userRepository.find({relations: [
-                                        "department", 
-                                        "department.members", 
-                                        "department.subDepartments"
-                                    ]})
+        return await this.userRepository.find({
+                relations: [
+                    "department", 
+                    "department.members", 
+                    "department.subDepartments"
+                ]})
+    }
+
+    @Query(() => User, {nullable: true})
+    async me(@Ctx() ctx: AuthContext):Promise<User | null> {
+        const authUser = await User.findOne(ctx.req.session!.userId, {
+            relations: ["department", "department.subDepartments", "department.members"]
+        })
+        if(authUser) return authUser
+        else return null
     }
 
     @Mutation(() => User)
@@ -49,5 +61,21 @@ export class UserResolver {
             await this.userRepository.save(user)
             return user
         }
+    }
+
+    @Mutation(() => User, {nullable: true})
+    async login(
+        @Arg("data") data: LoginInput,
+        @Ctx() ctx: AuthContext
+        ): Promise<User | null>{
+        const {email, password} = data
+        const user = await this.userRepository.findOne({where: {email}})
+        if(user){
+            const isPasswordMatch = bcrypt.compareSync(password, user.password)
+            if(isPasswordMatch){
+                ctx.req.session!.userId = user._id
+                return user
+            } else return null
+        } else return null
     }
 }
